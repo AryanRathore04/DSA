dbffe059bf2452dd1fe87486008bf4036e326ee9
AryanRathore04 <aryan4meu@gmail.com>
2026-05-05 22:33:53 +0530

    solve one question of selection sort and learn something about the bubble sort

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -1,32 +1,39 @@
+// Write a program to sort an array of integers in ascending order using selection sort algorithm.
+
 #include <iostream>  // input/output
 #include <vector>    // dynamic arrays
 #include <algorithm> // sort, max, etc.
 using namespace std;
 
 void selection_sort(int arr[], int n){
-    for(int i = 0; i<=n-2; i++){
+    for(int i = 0; i <= n-2; i++){
         int mini = i;
-        for(int j = i; j<=n-1; j++){
-            if(arr[j] < arr[mini]) {
+        for(int j = i; j <= n-1; j++){
+            if(arr[j] < arr[mini]){
                 mini = j;
             }
         }
+
         int temp = arr[mini];
         arr[mini] = arr[i];
         arr[i] = temp;
     }
 }
 
-int main(){
+int main (){
     int n;
     cin >> n;
     int arr[n];
-    for(int i = 0; i<n; i++) cin >> arr[i];
-    selection_sort(arr, n);
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
 
-    for(int i = 0; i<n; i++){
+    selection_sort(arr, n);
+    for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
 
     return 0;
+    
+    
 }
