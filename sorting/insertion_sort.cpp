eef2bfa970289aa9ca6e5defc33585f26828e62e
AryanRathore04 <aryan4meu@gmail.com>
2026-05-07 14:51:27 +0530

    Solve the first question of the insertion sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,33 @@
+// Write a program to sort an array of integers in ascending order using bubble insertion algorithm.
+
+#include <iostream>  // input/output
+#include <vector>    // dynamic arrays
+#include <algorithm> // sort, max, etc.
+using namespace std;
+
+void insertion_sort(int arr[], int n){
+    for(int i = 0; i <= n-1; i++){
+       int j = i;
+       while(j > 0 && arr[j-1] > arr[j]){
+        int temp = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+        j--;
+       }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    insertion_sort(arr, n);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
