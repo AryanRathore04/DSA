cdfc852e6e5a76300897a0e10811be89c4fc04fe
AryanRathore04 <aryan4meu@gmail.com>
2026-05-10 18:40:50 +0530

    Remove the duplicate array element

---
+++ array/remove_duplicate.cpp
@@ -0,0 +1,36 @@
+// Remove the duplicated number from the array
+ 
+#include <iostream>  // input/output
+#include <vector>    // dynamic arrays
+#include <algorithm> // sort, max, etc.
+using namespace std;
+
+int removeDuplicates(vector<int> &arr, int n){
+    int i = 0;
+    for(int j = 1; j < n; j++){
+        if(arr[i] != arr[j]){
+            arr[i+1] = arr[j];
+            i++;
+        }
+    }
+    return i + 1;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+   int ans = removeDuplicates(arr, n);
+   cout << ans << endl;
+    for(int i = 0; i < ans; i++){
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
