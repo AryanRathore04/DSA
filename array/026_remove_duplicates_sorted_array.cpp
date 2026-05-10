3b502deeaf2b3eb8d87af40a3b1acfb3f51df2d7
AryanRathore04 <aryan4meu@gmail.com>
2026-05-10 20:22:09 +0530

    First LeetCode question which is remove duplicates sorted array

---
+++ array/026_remove_duplicates_sorted_array.cpp
@@ -0,0 +1,28 @@
+// LeetCode 26
+// Remove Duplicates from Sorted Array
+// Approach: Two Pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <vector>;
+using namespace std;
+
+
+    class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int i = 0;
+
+        for(int j = 1; j < nums.size(); j++){
+
+            if(nums[i] != nums[j]){
+
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+
+        return i + 1;
+    }
+};
