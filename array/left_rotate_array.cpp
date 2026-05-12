e821f950a3f665185665e4629f05b0e652ade66f
AryanRathore04 <aryan4meu@gmail.com>
2026-05-12 09:16:30 +0530

    Left rotate the array

---
+++ array/left_rotate_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>  // input/output
+#include <vector>    // dynamic arrays
+#include <algorithm> // sort, max, etc.
+using namespace std;
+
+void leftRotate(vector<int> &arr, int n){
+    int temp = arr[0];
+    for(int i = 1; i < n; i++){
+        arr[i-1] = arr[i];
+    }
+
+    arr[n-1] = temp;
+
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    leftRotate(arr, n);
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
