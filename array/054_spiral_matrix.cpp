03b671a682b5b590cd4e3e58bb386fb8dbcc66aa
AryanRathore04 <aryan4meu@gmail.com>
2026-07-21 23:51:54 +0530

    Leetcode 54 Spiral Matrix

--- array/054_spiral_matrix.cpp
+++ array/054_spiral_matrix.cpp
@@ -1,4 +1,4 @@
-// Spiral Traversal
+// Leetcode 54. Spiral Matrix
 // Time Complexity: O(n*m)
 // Space Complexity: O(n*m)
 
@@ -17,53 +17,43 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> spiralMatrix(vector<vector<int>> &mat)
-    {
-        int n = mat.size();
-        int m = mat[0].size();
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
 
         int left = 0, right = m - 1;
         int top = 0, bottom = n - 1;
+
         vector<int> ans;
 
-        while (top <= bottom && left <= right)
-        {
-            // Left to Right
-            for (int i = left; i <= right; i++)
-            {
-                ans.push_back(mat[top][i]);
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                ans.push_back(matrix[top][i]);
             }
             top++;
 
-            // Top to bottom
-            for (int i = top; i <= bottom; i++)
-            {
-                ans.push_back(mat[i][right]);
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
             }
             right--;
 
-            if (top <= bottom)
-            {
-                // Right to left
-                for (int i = right; i >= left; i--)
-                {
-                    ans.push_back(mat[bottom][i]);
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
 
-            if (left <= right)
-            {
-                // Bottom to top
-                for (int i = bottom; i >= top; i--)
-                {
-                    ans.push_back(mat[i][left]);
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
                 }
                 left++;
             }
         }
+
+        return ans;
     }
 };
