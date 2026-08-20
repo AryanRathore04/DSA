cc93e1cb0b0ec117d3d5bc11cde7008437e2b428
AryanRathore04 <aryan4meu@gmail.com>
2026-08-20 15:16:16 +0530

    Leetcode 81 Search in Rotated Sorted array II

--- binary/081_search_in_rotated_sorted_array_II.cpp
+++ binary/081_search_in_rotated_sorted_array_II.cpp
@@ -1,45 +1,39 @@
-// Problem: Find an element in a rotated sorted array with duplicates
-
+// Leetcode 81. Search in Rotated Sorted Array II
 // Time Complexity: O(log n) in average case, O(n) in worst case
 // Space Complexity: O(1)
 
 // Approach: We can use binary search to find the element in the rotated sorted array. We will check if the left half or right half is sorted and then decide which half to search based on the value of the target element. If we encounter duplicates, we will shrink the search space by moving the low and high pointers.
 
-#include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
-    public:
-    bool searchInARotatedSortedArrayII(vector<int>& arr, int k){
-        int n = arr.size();
+public:
+    bool search(vector<int>& nums, int target) {
         int low = 0;
-        int high = n - 1;
+        int high = nums.size() - 1;
 
         while(low <= high){
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
 
-            if(arr[mid] == k){
-                return true;
-            }
+            if(nums[mid] == target) return true;
 
-            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+            if(nums[mid] == nums[low] && nums[mid] == nums[high]){
                 low++;
                 high--;
                 continue;
             }
-            // left half is sorted
-            else if(arr[low] <= arr[mid]){
-                if(k >= arr[low] && k <= arr[mid]){
+
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target <= nums[mid]){
                     high = mid - 1;
                 }
                 else{
                     low = mid + 1;
                 }
-                // right half is sorted
             }
             else{
-                if(k >= arr[mid] && k <= arr[high]){
+                if(target >= nums[mid] && target <= nums[high]){
                     low = mid + 1;
                 }
                 else{
@@ -47,20 +41,7 @@ class Solution {
                 }
             }
         }
+
         return false;
     }
 };
-
-int main(){
-    Solution obj;
-    vector<int> arr = {2, 5, 6, 0, 0, 1, 2};
-    int k = 0;
-    bool result = obj.searchInARotatedSortedArrayII(arr, k);
-    if(result){
-        cout << "Element found in the array." << endl;
-    }
-    else{
-        cout << "Element not found in the array." << endl;
-    }
-    return 0;
-}
