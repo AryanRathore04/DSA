96b28766fc3e45091a14f57f51e92c999727d396
AryanRathore04 <aryan4meu@gmail.com>
2026-09-11 15:09:33 +0530

    Added the comments in the leetcode 144

--- trees/144_binary_tree_preorder_traversal.cpp
+++ trees/144_binary_tree_preorder_traversal.cpp
@@ -1,3 +1,13 @@
+// Leetcode 144. Binary Tree Preorder Traversal
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+// Time Complexity: O(n), where n is the number of nodes in the binary tree
+// Space Complexity: O(h), where h is the height of the binary tree (due to
+
+// Approach: The pre-order traversal of a binary tree visits the nodes in the following order:
+// 1. Visit the root node
+// 2. Traverse the left subtree
+// 3. Traverse the right subtree
+
 #include <vector>
 #include <iostream>
 #include <stack>
@@ -8,6 +18,7 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
+
 
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
